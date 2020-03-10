a98e5182689e6fe9684873477cba95015bef40f6
bhaumikmistry <bmistry@medicacorp.com>
2020-03-10 11:32:23 -0400

    add new py files for actions

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import requests  # We are just importing this to prove the dependency installed correctly
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
-
+    version_file_name = os.environ["INPUT_VERSIONFILENAME"]
+    print(f'version updated!! in file {version_file_name}')
+    version_number_encapsulate_text = os.environ["INPUT_VERSIONNUMBERENCAPSULATETEXT"]
+    print(f'version updated!! in file {version_number_encapsulate_text}')
 
 if __name__ == "__main__":
     main()
